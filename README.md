a8e7f7eee7735c557bf2ba4e3789cb539731d91a
david zhao <david.zhao.blog@gmail.com>
2021-07-04 08:02:33 -0400

    Basic-1.2. material-ui/core
    Basic-1.2. material-ui/core
    npm install @material-ui/core
    npm install @material-ui/icons

--- README.md
+++ README.md
@@ -7,3 +7,6 @@ npx create-react-app react17mui4 --template typescript
 
 Basic-1.1.1. tsconfig.json
 
+Basic-1.2. material-ui/core
+npm install @material-ui/core
+npm install @material-ui/icons
--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,33 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Container from '@material-ui/core/Container';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
+import { Accordion, AccordionDetails, AccordionSummary } from '@material-ui/core';
+import { ExpandMore } from '@material-ui/icons';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container maxWidth="sm">
+      <Box my={4}>
+        {/* Basic-1.1. create-react-app */}
+        <Accordion>
+          <AccordionSummary expandIcon={<ExpandMore />} aria-controls="panel2a-content" id="panel2a-header-basic-1.1">
+            <Typography >Basic-1.1. create-react-app</Typography>
+          </AccordionSummary>
+          <AccordionDetails>
+            <Typography>React 17.0.2</Typography>
+          </AccordionDetails>
+        </Accordion>
+        {/* Basic-1.2 material-ui/core */}
+        <Accordion>
+          <AccordionSummary expandIcon={<ExpandMore />} aria-controls="panel2a-content" id="panel2a-header-basic-1.2">
+            <Typography>Basic-1.2 material-ui/core</Typography>
+          </AccordionSummary>
+          <AccordionDetails>
+            <Typography>Material-UI ^4.11.4</Typography>
+          </AccordionDetails>
+        </Accordion>
+      </Box>
+    </Container>
   );
 }
-
-export default App;
